5e5c93b8f365477b9fba3fb339f1fa63536276f9
Andrey Tarantsov <andrey@tarantsov.com>
2015-10-20 00:08:00 +0600

    🚀 Events

---
+++ Source/Events.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+public protocol EmitterType: class {
+    var _listenerStorage: ListenerStorage { get }
+}
+
+public protocol EventType {
+    static var eventName: String { get }
+}
+
+public protocol EventListenerType: class {
+    func handle(sender: EmitterType, _ payload: EventType)
+}
+
+public class ListenerStorage {
+
+    private static let globalEvents = ListenerStorage()
+    private static var globalEventsSpinlock = OS_SPINLOCK_INIT
+
+    private var listeners: [String: [EventListenerType]] = [:]
+
+    func handle(sender: EmitterType, _ payload: EventType) {
+        let event = payload.dynamicType.eventName
+        if let array = listeners[event] {
+            for listener in array {
+                listener.handle(sender, payload)
+            }
+        }
+    }
+
+    func add(event: String, listener: EventListenerType) {
+        if var array = listeners[event] {
+            array.append(listener)
+            listeners[event] = array
+        } else {
+            listeners[event] = [listener]
+        }
+    }
+
+    func remove(event: String, listener: EventListenerType) {
+        if var array = listeners[event] {
+            let idx = array.indexOf { $0 === listener }
+            if let idx = idx {
+                array.removeAtIndex(idx)
+                listeners[event] = array
+            }
+        }
+    }
+
+}
+
+public extension EmitterType {
+
+    public func emit<Event: EventType>(event: Event) {
+        _listenerStorage.handle(self, event)
+    }
+
+}
+
+public extension EventType {
+
+    public static var eventName: String {
+        return String(reflecting: self)
+    }
+
+    public var eventName: String {
+        return self.dynamicType.eventName
+    }
+
+    public static func addListener(listener: EventListenerType) {
+        fatalError("Not implemented")
+//        OSSpinLockLock(&ListenerStorage.globalEventsSpinlock)
+//        OSSpinLockUnlock(&ListenerStorage.globalEventsSpinlock)
+    }
+
+    public static func removeListener(listener: EventListenerType) {
+        fatalError("Not implemented")
+    }
+
+}
--- Source/Observation.swift
+++ Source/Observation.swift
@@ -1,66 +1,19 @@
 import Foundation
 import ObjectiveC
 
+public protocol ObserverType: class {
+}
+
 public struct Observation {
-    private var observations: [ObservationProxy] = []
+    private var observers: [ObserverType] = []
 
-    public init() {
-    }
+    public init() {}
 
-    public mutating func on(name: String, object: AnyObject? = nil, _ block: (NSNotification) -> Void) {
-        observations.append(NotificationObservationProxy(name: name, object: object, block: block))
-    }
-
-    public mutating func on <T: AnyObject> (name: String, object: AnyObject? = nil, _ observer: T, _ block: (T) -> (NSNotification) -> Void) {
-        weak var weakObserver: T? = observer
-        on(name, object: object) { notification in
-            if let observer = weakObserver {
-                block(observer)(notification)
-            }
-        }
-    }
-
-    public mutating func on <T: AnyObject> (name: String, object: AnyObject? = nil, _ observer: T, _ block: (T) -> () -> Void) {
-        weak var weakObserver: T? = observer
-        on(name, object: object) { notification in
-            if let observer = weakObserver {
-                block(observer)()
-            }
-        }
+    public mutating func add(observer: ObserverType) {
+        observers.append(observer)
     }
 
     public mutating func unobserve() {
-        observations = []
+        observers = []
     }
-}
-
-private class ObservationProxy : NSObject {
-}
-
-private class NotificationObservationProxy : ObservationProxy {
-    let name: String  // mostly for debugging purposes
-    let block: (NSNotification) -> Void
-
-    init(name: String, object: AnyObject? = nil, block: (NSNotification) -> Void) {
-        self.name = name
-        self.block = block
-        super.init()
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "trigger:", name: name, object: object)
-    }
-
-    deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
-    }
-
-    @objc func trigger(notification: NSNotification) {
-        block(notification)
-    }
-}
-
-extension NSObject {
-
-    public func postNotification(name: String) {
-        NSNotificationCenter.defaultCenter().postNotificationName(name, object: self)
-    }
-
 }
